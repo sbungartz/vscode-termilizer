079832c3f5c0428dcb4a734834e2529f3507c799
Simon Bungartz <simon.bungartz@zweitag.de>
2023-01-05 16:22:39 +0100

    Allow running block under cursor (#1)
    * Simon did a WIP 😱
    * Gather lines and paste to terminal
    
    Co-authored-by: Frederik Delißen <frederik.delissen@zweitag.de>

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,34 @@ import * as vscode from 'vscode';
 const getTextToRun = (editor: vscode.TextEditor) => {
 	if (editor.selection.isEmpty) {
 		const lineIndex = editor.selection.active.line;
-		return editor.document.lineAt(lineIndex).text;
+		return editor.document.lineAt(lineIndex).text.trim();
 	} else {
 		return editor.document.getText(editor.selection);
 	}
+};
+
+const findFirstLineIndexOfBlock = (document: vscode.TextDocument, beforeLineIndex: number): number => {
+	if (beforeLineIndex === 0) { return 0; }
+	if (document.lineAt(beforeLineIndex - 1).isEmptyOrWhitespace) { return beforeLineIndex; }
+	return findFirstLineIndexOfBlock(document, beforeLineIndex - 1);
+};
+
+const getBlockToRun = (editor: vscode.TextEditor) => {
+	const cursorLineIndex = editor.selection.active.line;
+	if (editor.document.lineAt(cursorLineIndex).isEmptyOrWhitespace) {
+		return "";
+	}
+
+	const startLineIndex = findFirstLineIndexOfBlock(editor.document, cursorLineIndex);
+	const lines = [];
+
+	for (let index = startLineIndex; index < editor.document.lineCount; index++) {
+		const line = editor.document.lineAt(index);
+		if(line.isEmptyOrWhitespace){ break; }
+		lines.push(line.text);
+	}
+
+	return lines.join('\n');
 };
 
 export function activate(context: vscode.ExtensionContext) {
@@ -23,6 +47,20 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	let disposable2 = vscode.commands.registerCommand('vs-code-slime.pasteBlockToTerminal', () => {
+    const editor = vscode.window.activeTextEditor;
+		const terminal = vscode.window.activeTerminal;
+		if (!editor || !terminal) {
+			vscode.window.showInformationMessage("Need to have an active editor and an active terminal");
+			return;
+		}
+
+		const textToRun = getBlockToRun(editor);
+		terminal.sendText(textToRun);
+	});
+
+	context.subscriptions.push(disposable2);
 }
 
 export function deactivate() {}
